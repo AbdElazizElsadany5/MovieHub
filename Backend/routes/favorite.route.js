b8e99214247377fd1f266f4f2829d45cded8ef73
zizoelsadany <zizoelsadany7@gmail.com>
2026-08-23 17:42:17 +0300

    Edit favorite.router

--- Backend/routes/favorite.route.js
+++ Backend/routes/favorite.route.js
@@ -1,20 +1,7 @@
 const express = require("express");
-
 const router = express.Router();
-
 const auth = require("../middlewares/auth");
-
-const {
-    addFavorite,
-    removeFavorite,
-    getFavorites
-} = require("../controllers/favorite.controller");
-
-router.route("/")
-    .post(auth, addFavorite)
-    .get(auth, getFavorites);
-
-router.route("/:movieId")
-    .delete(auth, removeFavorite);
-
+const { addFavorite, removeFavorite, getFavorites } = require("../controllers/favorite.controller");
+router.route("/").post(auth, addFavorite).get(auth, getFavorites);
+router.route("/:movieId").delete(auth, removeFavorite);
 module.exports = router;
