023c6dbfbb971cac3edbea221158175a5d545515
Abdelrahman Hamdalla <abdelrhmanhamdllah@gmail.com>
2026-08-23 16:09:56 +0300

    Refactor movie route to include CRUD operations

--- Backend/routes/movie.route.js
+++ Backend/routes/movie.route.js
@@ -1,4 +1,29 @@
-const express = require('express');
+const express = require("express");
+const movieController = require("../controllers/movie.controller");
+const auth = require("../middlewares/auth");
+const restrictTo = require("../middlewares/restrictTo");
 const router = express.Router();
 
+
+router.get("/", movieController.getAllMovies);
+router.get("/:id", movieController.getMovie);
+
+router.post(
+    "/",
+    auth,
+    restrictTo("admin"),
+    movieController.addMovie
+);
+router.put(
+    "/:id",
+    auth,
+    restrictTo("admin"),
+    movieController.updateMovie
+);
+router.delete(
+    "/:id",
+    auth,
+    restrictTo("admin"),
+    movieController.deleteMovie
+);
 module.exports = router;
