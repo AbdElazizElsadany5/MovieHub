bf9b2a0984333d7a813316e640403fdc64fae0da
zizoelsadany <zizoelsadany7@gmail.com>
2026-08-23 17:41:42 +0300

    Edit App

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,9 @@ const authRoutes = require('./routes/auth.route');
 const userRoutes = require('./routes/user.route');
 const movieRoutes = require('./routes/movie.route');
 const favoriteRoutes = require('./routes/favorite.route');
+const reviewRoutes = require('./routes/review.route');
+const globalError = require('./middlewares/globalError');
+const AppError = require('./utils/AppError');
 
 const app = express();
 app.use(express.json());
@@ -18,11 +21,12 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/movies', movieRoutes);
 app.use('/api/favorites', favoriteRoutes);
+app.use('/api/reviews', reviewRoutes);
 
-app.all('*splat', (req, res) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
-  });
+app.use((req, res, next) => {
+  next(new AppError(404, `Can't find ${req.originalUrl} on this server!`));
 });
+
+app.use(globalError);
+
 module.exports = app;
