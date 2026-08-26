762b13a7b1d10626e532f7f2b10d8422a9d1d29e
Abdelrahman Hamdalla <abdelrhmanhamdllah@gmail.com>
2026-08-27 02:40:30 +0300

    Add query parser setting to server configuration

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,11 @@ const chalk = require("chalk")
 const {setServers} = require("dns/promises")
 setServers(["8.8.8.8" , "8.8.4.4"])
 const app = require("./app")
+app.set("query parser","extended")
 const connectDB = require("./config/connectDB")
 connectDB()
 const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log(chalk.bgGreen(`Server is running at: http://localhost:${port}`));
 })
-module.exports = app;
+module.exports = app;
