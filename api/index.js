ef0f15b1cd40eae70d08eec4658ac8f53e8695d0
zizoelsadany <zizoelsadany7@gmail.com>
2026-08-28 17:56:02 +0300

    Migrate to modern Vercel zero-config deployment architecture

---
+++ api/index.js
@@ -0,0 +1,11 @@
+const app = require('./Backend/app');
+const connectDB = require('./Backend/config/connectDB');
+
+module.exports = async (req, res) => {
+  try {
+    await connectDB();
+  } catch (err) {
+    console.error('Serverless DB connection error:', err);
+  }
+  return app(req, res);
+};
