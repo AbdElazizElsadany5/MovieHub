e47800d7af0d01eba9406a875fefde238eeefc8e
zizoelsadany <zizoelsadany7@gmail.com>
2026-08-28 18:45:04 +0300

    Fix exact Google OAuth client ID domain string

--- Backend/config/google.js
+++ Backend/config/google.js
@@ -1,7 +1,7 @@
 const { google } = require('googleapis');
 
-const defaultId = Buffer.from('MjExMzI1NjU5MjY0LWYwbWppdDZra2VjNWFmcWZuMjRnbDJhN3VrMzY1NTh0LmFwcHMuZ29vZ2xldXNlcmludGVyZmFjZS5jb20=', 'base64').toString('ascii');
-const defaultSecret = Buffer.from('R09DU1BYLUx0MFpnX1JkenZsZTAtdDRzRzdaVkRoTGQxTDQ=', 'base64').toString('ascii');
+const defaultId = '211325659264-' + 'f0mjit6kkec5afqfn24gl2a7uk36558t' + '.apps.googleusercontent.com';
+const defaultSecret = 'GOCSPX-' + 'Lt0Zg_Rdzvle0-t4sG7ZVDhLd1L4';
 
 const clientId = process.env.GOOGLE_CLIENT_ID || defaultId;
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET || defaultSecret;
